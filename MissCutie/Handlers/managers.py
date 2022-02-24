f8037adc53459ead3a641f6c5ce4e1d3834670e6
saifalisew1508 <80207551+saifalisew1508@users.noreply.github.com>
2022-02-24 16:01:41 +0530

    Update managers.py

--- MissCutie/Handlers/managers.py
+++ MissCutie/Handlers/managers.py
@@ -1,6 +1,6 @@
-import Yone.Database.blacklistusers_sql as sql
-from Yone import ALLOW_EXCL
-from Yone import DEV_USERS, INSPECTOR, REQUESTER
+import MissCutie.Database.blacklistusers_sql as sql
+from MissCutie import ALLOW_EXCL
+from MissCutie import DEV_USERS, INSPECTOR, REQUESTER
 from itertools import chain
 from telegram import Update
 from telegram.ext import CommandHandler, MessageHandler, RegexHandler, Filters
