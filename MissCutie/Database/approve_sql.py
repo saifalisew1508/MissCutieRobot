d577e03737d654399b1d76d113c92b310db48e02
saifalisew1508 <80207551+saifalisew1508@users.noreply.github.com>
2022-02-24 10:31:40 +0530

    Update approve_sql.py

--- MissCutie/Database/approve_sql.py
+++ MissCutie/Database/approve_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, BigInteger
 
-from Yone.Database import BASE, SESSION
+from MissCutie.Database import BASE, SESSION
 
 
 class Approvals(BASE):
