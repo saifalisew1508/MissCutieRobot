24b65a400a2f7c5b003b3d3c47aa58447a229522
saifalisew1508 <80207551+saifalisew1508@users.noreply.github.com>
2022-02-24 16:02:05 +0530

    Update misc.py

--- MissCutie/Handlers/misc.py
+++ MissCutie/Handlers/misc.py
@@ -1,7 +1,7 @@
 from math import ceil
 from typing import Dict, List
 from telegram import InlineKeyboardButton, MAX_MESSAGE_LENGTH, ParseMode, Bot
-from Yone import NO_LOAD
+from MissCutie import NO_LOAD
 from telegram.error import TelegramError
 
 
