a6bfbb4e75b2840c2c37081b21583b9cc026de03
saifalisew1508 <80207551+saifalisew1508@users.noreply.github.com>
2022-02-27 17:26:26 +0530

    Update wiki.py

--- MissCutie/Plugins/Tools/wiki.py
+++ MissCutie/Plugins/Tools/wiki.py
@@ -1,6 +1,6 @@
 import wikipedia
-from Yone import dispatcher
-from Yone.Plugins.disable import DisableAbleCommandHandler
+from MissCutie import dispatcher
+from MissCutie.Plugins.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 from wikipedia.exceptions import DisambiguationError, PageError
