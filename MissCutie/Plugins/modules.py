dd9be9fc6dfebdbf5951bcb335b4c12c8e2bb1e6
saifalisew1508 <80207551+saifalisew1508@users.noreply.github.com>
2022-02-27 17:32:54 +0530

    Update modules.py

--- MissCutie/Plugins/modules.py
+++ MissCutie/Plugins/modules.py
@@ -1,8 +1,8 @@
 import importlib
 import collections
 
-from Yone import dispatcher, telethn
-from Yone.__help__ import (
+from MissCutie import dispatcher, telethn
+from MissCutie.__help__ import (
     CHAT_SETTINGS,
     DATA_EXPORT,
     DATA_IMPORT,
@@ -13,7 +13,7 @@ from Yone.__help__ import (
     USER_INFO,
     USER_SETTINGS
 )
-from Yone.Handlers.validation import dev_plus, sudo_plus
+from MissCutie.Handlers.validation import dev_plus, sudo_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
