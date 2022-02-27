12fab1dcce65c40dab3be2cb009d6c14c21792d2
saifalisew1508 <80207551+saifalisew1508@users.noreply.github.com>
2022-02-27 18:36:47 +0530

    Update disable.py

--- MissCutie/Plugins/disable.py
+++ MissCutie/Plugins/disable.py
@@ -160,7 +160,7 @@ if is_module_loaded(FILENAME):
         args = context.args
         chat = update.effective_chat
         if len(args) >= 1:
-            disable_module = "YoneRobot.modules." + args[0].rsplit(".", 1)[0]
+            disable_module = "MissCutieRobot.modules." + args[0].rsplit(".", 1)[0]
 
             try:
                 module = importlib.import_module(disable_module)
