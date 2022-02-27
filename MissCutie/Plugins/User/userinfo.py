29ca9cc5bedabea52f36c1de330d4f803f3508ba
saifalisew1508 <80207551+saifalisew1508@users.noreply.github.com>
2022-02-27 18:42:57 +0530

    Update userinfo.py

--- MissCutie/Plugins/User/userinfo.py
+++ MissCutie/Plugins/User/userinfo.py
@@ -563,7 +563,7 @@ Examples:
  ‣ `/ginfo`*:* get information about a Group. 
  
 *What is that health thingy?*
- Come and see [HP System explained](https://t.me/Yone_Updates)
+ Come and see [HP System explained](https://t.me/MissCutie_Updates)
 """
 __mod_name__ = "Infos"
 __command_list__ = ["setbio", "bio", "setme", "me", "info"]
