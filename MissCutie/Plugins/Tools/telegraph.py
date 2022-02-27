2a247bba8cde4da2efe73f451682299dc975a745
saifalisew1508 <80207551+saifalisew1508@users.noreply.github.com>
2022-02-27 17:25:25 +0530

    Update telegraph.py

--- MissCutie/Plugins/Tools/telegraph.py
+++ MissCutie/Plugins/Tools/telegraph.py
@@ -1,11 +1,11 @@
-from Yone import telethn as tbot
+from MissCutie import telethn as tbot
 TMP_DOWNLOAD_DIRECTORY = "./"
 from telethon import events
 import os
 from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
-babe = "YONE"
+babe = "MissCutie"
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=babe)
 auth_url = r["auth_url"]
