0a4303d3c3e801a73ee927176c58a367920922d1
SAIF ALI <sachinsaif03@gmail.com>
2023-01-13 21:50:37 +0530

    Update anti_channel.py

--- MissCutie/Plugins/Admin/anti_channel.py
+++ MissCutie/Plugins/Admin/anti_channel.py
@@ -75,13 +75,49 @@ def antichannel(update: Update, context: CallbackContext):
     keyboard = InlineKeyboardMarkup([[
         InlineKeyboardButton(
             text="Enable",
-            callback_data="add_chat({})")],
+            callback_data="anti_c_mode_on")],
        [
         InlineKeyboardButton(
             text="Disable",
-            callback_data="rm_chat({})")]])
+            callback_data="anti_c_mode_off")]])
     message.reply_text(
         msg,
         reply_markup=keyboard,
         parse_mode=ParseMode.HTML,
     )
+
+
+
+__help__ = f"""
+Through this menu you can set a punishment for users who write in the group masquerading as a channel.
+
+ℹ️ Telegram allows each user to write to the group by hiding through a channel they own.
+
+👮🏻‍♂️ It's not possible to know which user is writing via a channel and if it is an administrator: this block will apply to whoever writes via a channel.
+
+🏃🏻 If this option is active, a user who was writing via a channel will only be able to continue writing to the group but only via his real identity and no longer via other channels.
+*Commands:*
+*Admins only:*
+➢ `/antichannel`*:* Shows Anti-Channel control panel
+"""
+
+
+
+ANTICHANNEL_PANEL_COMMAND_HANDLER = CommandHandler("antichannel", run_async=True)
+ANTICHANNEL_ON_COMMAND_HANDLER = CommandHandler("antichannelon", anti_c_mode_on, run_async=True)
+ANTICHANNEL_OFF_COMMAND_HANDLER = CommandHandler("antichanneloff", anti_c_mode_off, run_async=True)
+
+
+# Filters for ignoring #note messages, !commands and sed.
+
+dispatcher.add_handler(ANTICHANNEL_PANEL_COMMAND_HANDLER)
+dispatcher.add_handler(ANTICHANNEL_ON_COMMAND_HANDLER)
+dispatcher.add_handler(ANTICHANNEL_OFF_COMMAND_HANDLER)
+
+__mod_name__ = "Anti-Channel"
+__command_list__ = ["antichannel", "antichannelon", "antichanneloff"]
+__handlers__ = [
+    ANTICHANNEL_PANEL_COMMAND_HANDLER,
+    ANTICHANNEL_ON_COMMAND_HANDLER,
+    ANTICHANNEL_OFF_COMMAND_HANDLER,
+]
