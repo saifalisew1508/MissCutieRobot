096da8cb2532ff16586caeb76de86d90b4353f51
SAIF ALI <sachinsaif03@gmail.com>
2023-01-14 17:13:08 +0530

    Update chatbot_sql.py

--- MissCutie/Database/chatbot_sql.py
+++ MissCutie/Database/chatbot_sql.py
@@ -1,44 +1,47 @@
 import threading
-
 from sqlalchemy import Column, String
 from MissCutie.Database import BASE, SESSION
 
 
-class MissCutieChats(BASE):
-    __tablename__ = "misscutie_chats"
+class KukiChats(BASE):
+    __tablename__ = "kuki_chats"
     chat_id = Column(String(14), primary_key=True)
 
     def __init__(self, chat_id):
         self.chat_id = chat_id
 
-MissCutieChats.__table__.create(checkfirst=True)
+
+KukiChats.__table__.create(checkfirst=True)
 INSERTION_LOCK = threading.RLock()
 
 
-def is_misscutie(chat_id):
+def is_kuki(chat_id):
     try:
-        chat = SESSION.query(MissCutieChats).get(str(chat_id))
+        chat = SESSION.query(KukiChats).get(str(chat_id))
         return bool(chat)
     finally:
         SESSION.close()
 
-def set_misscutie(chat_id):
+
+def set_kuki(chat_id):
     with INSERTION_LOCK:
-        misscutiechat = SESSION.query(MissCutieChats).get(str(chat_id))
-        if not misscutiechat:
-            misscutiechat = MissCutieChats(str(chat_id))
-        SESSION.add(misscutiechat)
+        kukichat = SESSION.query(KukiChats).get(str(chat_id))
+        if not kukichat:
+            kukichat = KukiChats(str(chat_id))
+        SESSION.add(kukichat)
         SESSION.commit()
 
-def rem_misscutie(chat_id):
+
+def rem_kuki(chat_id):
     with INSERTION_LOCK:
-        misscutiechat = SESSION.query(MissCutieChats).get(str(chat_id))
-        if misscutiechat:
-            SESSION.delete(misscutiechat)
+        kukichat = SESSION.query(KukiChats).get(str(chat_id))
+        if kukichat:
+            SESSION.delete(kukichat)
         SESSION.commit()
 
-def get_all_misscutie_chats():
+
+def get_all_kuki_chats():
     try:
-        return SESSION.query(MissCutieChats.chat_id).all()
+        return SESSION.query(KukiChats.chat_id).all()
     finally:
         SESSION.close()
