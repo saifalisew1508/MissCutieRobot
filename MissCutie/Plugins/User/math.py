f59d5ec89a39c6877128156cce62b2d4c15cb6d4
SAIF ALI <sachinsaif03@gmail.com>
2023-01-18 06:56:47 +0530

    Update math.py

--- MissCutie/Plugins/User/math.py
+++ MissCutie/Plugins/User/math.py
@@ -1,11 +1,12 @@
 import math
-from typing import List
-import requests
+
 import pynewtonmath as newton
+from telegram import Update
+from telegram.ext import CallbackContext, run_async
+
 from MissCutie import dispatcher, BOT_ID, BOT_NAME, BOT_USERNAME
 from MissCutie.Plugins.disable import DisableAbleCommandHandler
-from telegram import Update
-from telegram.ext import CallbackContext, CommandHandler, run_async
+
 
 
 def simplify(update: Update, context: CallbackContext):
@@ -14,10 +15,12 @@ def simplify(update: Update, context: CallbackContext):
     message.reply_text(newton.simplify("{}".format(args[0])))
 
 
+
 def factor(update: Update, context: CallbackContext):
     args = context.args
     message = update.effective_message
     message.reply_text(newton.factor("{}".format(args[0])))
+
 
 
 def derive(update: Update, context: CallbackContext):
@@ -26,10 +29,12 @@ def derive(update: Update, context: CallbackContext):
     message.reply_text(newton.derive("{}".format(args[0])))
 
 
+
 def integrate(update: Update, context: CallbackContext):
     args = context.args
     message = update.effective_message
     message.reply_text(newton.integrate("{}".format(args[0])))
+
 
 
 def zeroes(update: Update, context: CallbackContext):
@@ -38,10 +43,12 @@ def zeroes(update: Update, context: CallbackContext):
     message.reply_text(newton.zeroes("{}".format(args[0])))
 
 
+
 def tangent(update: Update, context: CallbackContext):
     args = context.args
     message = update.effective_message
     message.reply_text(newton.tangent("{}".format(args[0])))
+
 
 
 def area(update: Update, context: CallbackContext):
@@ -50,10 +57,12 @@ def area(update: Update, context: CallbackContext):
     message.reply_text(newton.area("{}".format(args[0])))
 
 
+
 def cos(update: Update, context: CallbackContext):
     args = context.args
     message = update.effective_message
     message.reply_text(math.cos(int(args[0])))
+
 
 
 def sin(update: Update, context: CallbackContext):
@@ -62,10 +71,12 @@ def sin(update: Update, context: CallbackContext):
     message.reply_text(math.sin(int(args[0])))
 
 
+
 def tan(update: Update, context: CallbackContext):
     args = context.args
     message = update.effective_message
     message.reply_text(math.tan(int(args[0])))
+
 
 
 def arccos(update: Update, context: CallbackContext):
@@ -74,10 +85,12 @@ def arccos(update: Update, context: CallbackContext):
     message.reply_text(math.acos(int(args[0])))
 
 
+
 def arcsin(update: Update, context: CallbackContext):
     args = context.args
     message = update.effective_message
     message.reply_text(math.asin(int(args[0])))
+
 
 
 def arctan(update: Update, context: CallbackContext):
@@ -86,10 +99,12 @@ def arctan(update: Update, context: CallbackContext):
     message.reply_text(math.atan(int(args[0])))
 
 
+
 def abs(update: Update, context: CallbackContext):
     args = context.args
     message = update.effective_message
     message.reply_text(math.fabs(int(args[0])))
+
 
 
 def log(update: Update, context: CallbackContext):
@@ -98,31 +113,32 @@ def log(update: Update, context: CallbackContext):
     message.reply_text(math.log(int(args[0])))
 
 
-
-__mod_name__ = "Math"
+__mod_name__ = "Maths"
 
 
 __help__ = """
 Solves complex math problems using https://newton.now.sh
- - /math: Simplify `/simplify 2^2+2(2)`
- - /factor: Factor `/factor x^2 + 2x`
- - /derive: Derive `/derive x^2+2x`
- - /integrate: Integrate `/integrate x^2+2x`
- - /zeroes: Find 0's `/zeroes x^2+2x`
- - /tangent: Find Tangent `/tangent 2lx^3`
- - /area: Area Under Curve `/area 2:4lx^3`
- - /cos: Cosine `/cos pi`
- - /sin: Sine `/sin 0`
- - /tan: Tangent `/tan 0`
- - /arccos: Inverse Cosine `/arccos 1`
- - /arcsin: Inverse Sine `/arcsin 0`
- - /arctan: Inverse Tangent `/arctan 0`
- - /abs: Absolute Value `/abs -1`
- - /log: Logarithm `/log 2l8`
-__Keep in mind__: To find the tangent line of a function at a certain x value, send the request as c|f(x) where c is the given x value and f(x) is the function expression, the separator is a vertical bar '|'. See the table above for an example request.
+- /math*:* Math `/math 2^2+2(2)`
+- /factor*:* Factor `/factor x^2 + 2x`
+- /derive*:* Derive `/derive x^2+2x`
+- /integrate*:* Integrate `/integrate x^2+2x`
+- /zeroes*:* Find 0's `/zeroes x^2+2x`
+- /tangent*:* Find Tangent `/tangent 2lx^3`
+- /area*:* Area Under Curve `/area 2:4lx^3`
+- /cos*:* Cosine `/cos pi`
+- /sin*:* Sine `/sin 0`
+- /tan*:* Tangent `/tan 0`
+- /arccos*:* Inverse Cosine `/arccos 1`
+- /arcsin*:* Inverse Sine `/arcsin 0`
+- /arctan*:* Inverse Tangent `/arctan 0`
+- /abs*:* Absolute Value `/abs -1`
+- /log*:* Logarithm `/log 2l8`
+
+_Keep in mind_: To find the tangent line of a function at a certain x value, send the request as c|f(x) where c is the given x value and f(x) is the function expression, the separator is a vertical bar '|'. See the table above for an example request.
 To find the area under a function, send the request as c:d|f(x) where c is the starting x value, d is the ending x value, and f(x) is the function under which you want the curve between the two x values.
 To compute fractions, enter expressions as numerator(over)denominator. For example, to process 2/4 you must send in your expression as 2(over)4. The result expression will be in standard math notation (1/2, 3/4).
 """
+
 
 
 SIMPLIFY_HANDLER = DisableAbleCommandHandler("math", simplify, run_async=True)
@@ -139,7 +155,7 @@ ARCCOS_HANDLER = DisableAbleCommandHandler("arccos", arccos, run_async=True)
 ARCSIN_HANDLER = DisableAbleCommandHandler("arcsin", arcsin, run_async=True)
 ARCTAN_HANDLER = DisableAbleCommandHandler("arctan", arctan, run_async=True)
 ABS_HANDLER = DisableAbleCommandHandler("abs", abs, run_async=True)
-LOG_HANDLER = DisableAbleCommandHandler("abs", log, run_async=True)
+LOG_HANDLER = DisableAbleCommandHandler("log", log, run_async=True)
 
 dispatcher.add_handler(SIMPLIFY_HANDLER)
 dispatcher.add_handler(FACTOR_HANDLER)
@@ -156,6 +172,3 @@ dispatcher.add_handler(ARCSIN_HANDLER)
 dispatcher.add_handler(ARCTAN_HANDLER)
 dispatcher.add_handler(ABS_HANDLER)
 dispatcher.add_handler(LOG_HANDLER)
-
-
-__command_list__ = ["math", "factor", "derive", "integrate", "zeroes", "tangent", "area", "cos", "sin", "tan", "arccos", "arcsin", "arctan", "abs", "abs"]
