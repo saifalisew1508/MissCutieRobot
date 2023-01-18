559bc85673591ed18b34f83e18b61ff67318dd52
SAIF ALI <sachinsaif03@gmail.com>
2023-01-18 07:41:53 +0530

    Update admins.py

--- MissCutie/Handlers/pyrogram/admins.py
+++ MissCutie/Handlers/pyrogram/admins.py
@@ -3,18 +3,18 @@ from typing import Callable
 from pyrogram.enums import ChatMemberStatus
 from pyrogram.types import Message
 
-from MissCutie import OWNER_ID, pbot
+from MissCutie import OWNER_ID, DEV_USERS, pbot
 
 
 def can_change_info(func: Callable) -> Callable:
     async def non_admin(_, message: Message):
-        if message.from_user.id in DRAGONS:
+        if message.from_user.id in DEV_USERS:
             return await func(_, message)
 
         check = await pbot.get_chat_member(message.chat.id, message.from_user.id)
         if check.status not in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
             return await message.reply_text(
-                "» ʏᴏᴜ'ʀᴇ ɴᴏᴛ ᴀɴ ᴀᴅᴍɪɴ ʙᴀʙʏ, ᴘʟᴇᴀsᴇ sᴛᴀʏ ɪɴ ʏᴏᴜʀ ʟɪᴍɪᴛs."
+                "You are not admin"
             )
 
         admin = (
@@ -32,13 +32,13 @@ def can_change_info(func: Callable) -> Callable:
 
 def can_restrict(func: Callable) -> Callable:
     async def non_admin(_, message: Message):
-        if message.from_user.id in DEV_USERS:
+        if message.from_user.id in OWNER_ID:
             return await func(_, message)
 
         check = await pbot.get_chat_member(message.chat.id, message.from_user.id)
         if check.status not in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
             return await message.reply_text(
-                "» ʏᴏᴜ'ʀᴇ ɴᴏᴛ ᴀɴ ᴀᴅᴍɪɴ ʙᴀʙʏ, ᴘʟᴇᴀsᴇ sᴛᴀʏ ɪɴ ʏᴏᴜʀ ʟɪᴍɪᴛs."
+                "You are not admin."
             )
 
         admin = (
